f9072ba0ef1c4bcd9a3d5baed5f7777ad1b9cff1
kylemadkins <kyle.m.adkins@gmail.com>
2023-07-16 20:49:54 -0400

    Persist messages in Redis

--- ch4_quart.py
+++ ch4_quart.py
@@ -1,7 +1,7 @@
 import asyncio
 
-from quart import Quart, websocket, render_template
-from quart import g
+from quart import Quart, websocket, render_template, jsonify
+import redis.asyncio as redis
 
 
 app = Quart(__name__)
@@ -12,13 +12,24 @@ conn = set()
 
 @app.websocket("/ws")
 async def ws():
+    db = redis.Redis()
     conn.add(websocket._get_current_object())
     try:
         while True:
             msg = await websocket.receive()
+            await db.rpush("messages", msg)
+            await db.close()
             await asyncio.gather(*[c.send(msg) for c in conn])
     finally:
         conn.remove(websocket._get_current_object())
+
+
+@app.route("/messages")
+async def messages():
+    db = redis.Redis()
+    msgs = await db.lrange("messages", 0, -1)
+    await db.close()
+    return jsonify([m.decode("utf-8") for m in msgs])
 
 
 @app.route("/")
